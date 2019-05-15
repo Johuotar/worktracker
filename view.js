e5785f64c5f223f6a7439536d343ee25efbff010
Johuotar <jmh@dnainternet.net>
2019-05-15 12:01:41 +0300

    remove references to React Demo

--- view.js
+++ view.js
@@ -51,20 +51,20 @@ const e = React.createElement;
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+    this.state = { pressed: false };
   }
 
   render() {
-    if (this.state.liked) {
-      return 'You liked this.';
+    if (this.state.pressed) {
+      return 'You pressed a button.';
     }
 
     return e(
       'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
+      { onClick: () => this.setState({ pressed: true }) },
+      'testbutton'
     );
   }
 }
-const domContainer = document.querySelector('#like_button_container');
+const domContainer = document.querySelector('#projectview');
 ReactDOM.render(e(LikeButton), domContainer);
