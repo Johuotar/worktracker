8260ea627c8646062b7fd8d5b670defb5357125c
VadeSerg <50317784+VadeSerg@users.noreply.github.com>
2019-05-22 19:37:54 +0300

    VS: asiakasnäkymien haut, projekti ja projektin tehtävät

--- DBCreatev2.sql
+++ DBCreatev2.sql
@@ -1,6 +1,46 @@
 /* usefull scripts */
 
 
+/* VS 2019-05-22: asiakashenkilön projektinäkymän haku */
+SELECT a.projektiID, a.asiakasID, a.henkiloID
+,concat(c.sukunimi, ' ',c.etunimi) as ProjektiPaallikko
+,a.projekti, a.alkuPvm, a.loppuPvm, a.arkistointiPvm
+FROM projekti a
+inner join asiakas b on (a.asiakasID=b.asiakasID)
+inner join henkilo c on (a.henkiloID=c.henkiloID)
+inner join asiakas cc on (c.asiakasID=cc.asiakasID)
+inner join henkilonRooli e on (c.henkiloID=e.henkiloID and cc.asiakasID=e.asiakasID)
+inner join rooli f on (e.rooliID=f.rooliID)
+WHERE a.asiakasID=2 /* $asiakasID */
+and b.tyyppi='T'
+and cc.tyyppi='O'
+and f.rooli='Projektipäällikkö'
+and exists(select 1 from henkilo ab where a.asiakasID=ab.asiakasID and ab.henkiloID=11 /* $henkiloID */)
+;
+
+
+/* VS 2019-05-22: asiakashenkilön projektin tehtävän näkymän haku */
+SELECT a.projektinTehtavaID, a.asiakasID, a.projektiID
+,aa.projekti
+,a.henkiloID, concat(c.sukunimi, ' ',c.etunimi) as Tyontekija, 
+a.tehtava, a.alkuPvm, a.loppuPvm
+,a.tehtavanStatus, a.hyvaksymisPvm, a.arkistointiPvm
+FROM projektinTehtava a
+inner join projekti aa on (a.projektiID=aa.projektiID and a.asiakasID=aa.asiakasID)
+inner join asiakas b on (a.asiakasID=b.asiakasID)
+inner join henkilo c on (a.henkiloID=c.henkiloID)
+inner join asiakas cc on (c.asiakasID=cc.asiakasID)
+inner join henkilonRooli e on (c.henkiloID=e.henkiloID and cc.asiakasID=e.asiakasID)
+inner join rooli f on (e.rooliID=f.rooliID)
+WHERE a.asiakasID=2 /* $asiakasID */
+and b.tyyppi='T' /* tilaaja*/
+and cc.tyyppi='O' /* oma yritys */
+and f.rooli='Työntekijä'
+and exists(select 1 from henkilo ab where a.asiakasID=ab.asiakasID and ab.henkiloID=11 /* $henkiloID */)
+;
+ /* **********  */
+
+
 SELECT /* a.asiakasID, */
 a.nimi as AsiakkaanNimi
 /* ,a.tyyppi as asiakkaanTyyppi */
