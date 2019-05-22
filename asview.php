978b62196e01f8e62ff407377f8b61c6bf6814ca
VadeSerg <50317784+VadeSerg@users.noreply.github.com>
2019-05-22 20:55:05 +0300

    VS: asiakasnäkymän muutos

--- asview.php
+++ asview.php
@@ -31,7 +31,7 @@
 <h2>Your Projects and tasks:</h2>
     <div id="projectview"> </div>
       <?php
-
+	
         session_start();
 
         // print_r($_SESSION);
@@ -41,24 +41,27 @@
 
         require 'db_connection_config.php';  
       
-        //* Asiakkaan projektit */
-        $sql = "SELECT a.projektiID, a.asiakasID, b.nimi, a.henkiloID
-        ,concat(c.sukunimi, ' ' ,c.etunimi) as Projektipaalliko
-        ,a.projekti, a.alkuPvm, a.loppuPvm 
+        //* asiakashenkilön projektinäkymän haku */
+        $sql = "SELECT a.projektiID, a.asiakasID, a.henkiloID
+        ,concat(c.sukunimi, ' ',c.etunimi) as ProjektiPaallikko
+        ,a.projekti, a.alkuPvm, a.loppuPvm, a.arkistointiPvm
         FROM projekti a
-        inner join asiakas b on (a.asiakasID=b.asiakasID) /* haetaan asiakkaan nimi */
-        inner join henkilo c on (a.henkiloID=c.henkiloID) /* Projektipäällikkö */
-        WHERE exists(select 1 from projektinTehtava ac 
-                    where ac.henkiloID=$henkiloID
-                    and a.projektiID=ac.projektiID
-                    and a.asiakasID=ac.asiakasID
-                    )
+        inner join asiakas b on (a.asiakasID=b.asiakasID)
+        inner join henkilo c on (a.henkiloID=c.henkiloID)
+        inner join asiakas cc on (c.asiakasID=cc.asiakasID)
+        inner join henkilonRooli e on (c.henkiloID=e.henkiloID and cc.asiakasID=e.asiakasID)
+        inner join rooli f on (e.rooliID=f.rooliID)
+        WHERE a.asiakasID=$asiakasID
+        and b.tyyppi='T'
+        and cc.tyyppi='O'
+        and f.rooli='Projektipäällikkö'
+        and exists(select 1 from henkilo ab where a.asiakasID=ab.asiakasID and ab.henkiloID=$henkiloID)
         ;"; 
         
         $result = mysqli_query($mysqli, $sql) or die ("Bad Query: $sql");
       
           // taulun rakentaminen ja populointi
-          echo("<h3>Työntekijan projektit </h3>");
+          echo("<h3>Projektit </h3>");
           echo("<table border=1>");
           /* Get field information for all columns */
           $finfo = mysqli_fetch_fields($result);
@@ -71,91 +74,41 @@
           echo("</table> <br> <br>");
           // end of: taulun rakentaminen ja populointi 
 
-
-
-        $sql = "SELECT projektinTehtavaID, tehtava, alkuPvm, loppuPvm, tehtavanStatus, hyvaksymisPvm 
-        FROM projektinTehtava WHERE henkiloID=$henkiloID /* and projektiID=nn */"; 
-        // tähän henkiloID<>1 laitetaan kirjoutumisen yhteydessä tunnistettu henkiloID
-        
-        $result = mysqli_query($mysqli, $sql) or die ("Bad Query: $sql");
-          
-              // taulun rakentaminen ja populointi
-              echo("<h3>Työntekijan projektien tehtävät </h3>");
-              echo("<table border=1>");
-              /* Get field information for all columns */
-              $finfo = mysqli_fetch_fields($result);
-              foreach ($finfo as $val) {echo ('<th>'.$val->name.'</th>');}
-              while ($row = mysqli_fetch_assoc($result)) {
-                  echo ("<tr>");
-                  foreach ($finfo as $val) {echo ("<td>{$row[$val->name]}</td>");}
-                  echo ("</tr>");
-              }
-              echo("</table> <br> <br>");
-              // end of: taulun rakentaminen ja populointi 
-
-
-
-        //* Työntekijan projektit */
-        $sql = "SELECT a.projektiID, a.asiakasID, b.nimi, a.henkiloID
-        ,concat(c.sukunimi, ' ' ,c.etunimi) as Projektipaalliko
-        ,a.projekti, a.alkuPvm, a.loppuPvm 
-        FROM projekti a
-        inner join asiakas b on (a.asiakasID=b.asiakasID) /* haetaan asiakkaan nimi */
-        inner join henkilo c on (a.henkiloID=c.henkiloID) /* Projektipäällikkö */
-        WHERE exists(select 1 from projektinTehtava ac 
-                    where ac.henkiloID=$henkiloID
-                    and a.projektiID=ac.projektiID
-                    and a.asiakasID=ac.asiakasID
-                    )
+        //* asiakashenkilön projektin tehtävän näkymän haku */
+        $sql = "SELECT a.projektinTehtavaID, a.asiakasID, a.projektiID
+        ,aa.projekti
+        ,a.henkiloID, concat(c.sukunimi, ' ',c.etunimi) as Tyontekija, 
+        a.tehtava, a.alkuPvm, a.loppuPvm
+        ,a.tehtavanStatus, a.hyvaksymisPvm, a.arkistointiPvm
+        FROM projektinTehtava a
+        inner join projekti aa on (a.projektiID=aa.projektiID and a.asiakasID=aa.asiakasID)
+        inner join asiakas b on (a.asiakasID=b.asiakasID)
+        inner join henkilo c on (a.henkiloID=c.henkiloID)
+        inner join asiakas cc on (c.asiakasID=cc.asiakasID)
+        inner join henkilonRooli e on (c.henkiloID=e.henkiloID and cc.asiakasID=e.asiakasID)
+        inner join rooli f on (e.rooliID=f.rooliID)
+        WHERE a.asiakasID=$asiakasID
+        and b.tyyppi='T' /* tilaaja*/
+        and cc.tyyppi='O' /* oma yritys */
+        and f.rooli='Työntekijä'
+        and exists(select 1 from henkilo ab where a.asiakasID=ab.asiakasID and ab.henkiloID=$henkiloID)
         ;"; 
-        // tähän henkiloID=1 laitetaan kirjoutumisen yhteydessä tunnistettu henkiloID
         
         $result = mysqli_query($mysqli, $sql) or die ("Bad Query: $sql");
       
           // taulun rakentaminen ja populointi
-          echo("<h3>Työntekijan projektit </h3>");
-          // echo("<table border=1>");
-          // Get field information for all columns */
-          // $finfo = mysqli_fetch_fields($result);
+          echo("<h3>Projektien tehtävät </h3>");
+          echo("<table border=1>");
+
+          $finfo = mysqli_fetch_fields($result);
           foreach ($finfo as $val) {echo ('<th>'.$val->name.'</th>');}
           while ($row = mysqli_fetch_assoc($result)) {
-
-            echo("<table border=1>");
-            $finfo = mysqli_fetch_fields($result);
-            foreach ($finfo as $val) {echo ('<th>'.$val->name.'</th>');}             
               echo ("<tr>");
               foreach ($finfo as $val) {echo ("<td>{$row[$val->name]}</td>");}
-                echo ("</tr>");
-	            echo("</table>");
-	            $projektiID=$row['projektiID']; // otetaan talteen projektiID
-	        // sisälooppi
-	     	// tähän sisälooppi, tt:n pr. tehtävät
-            $sql1 = "SELECT projektinTehtavaID, tehtava, alkuPvm, loppuPvm, tehtavanStatus, hyvaksymisPvm 
-            FROM projektinTehtava WHERE henkiloID=$henkiloID and projektiID=$projektiID"; 
-            
-            $result1 = mysqli_query($mysqli, $sql1) or die ("Bad Query: $sql1");
-            
-                // taulun rakentaminen ja populointi
-                echo("<h3> ... projektien tehtävät </h3>");
-                echo("<table border=1>");
-                /* Get field information for all columns */
-                $finfo1 = mysqli_fetch_fields($result1);
-                foreach ($finfo1 as $val) {echo ('<th>'.$val->name.'</th>');}
-                while ($row1 = mysqli_fetch_assoc($result1)) {
-                    echo ("<tr>");
-                    foreach ($finfo1 as $val) {echo ("<td>{$row1[$val->name]}</td>");}
-                    echo ("</tr>");
-                }
-                echo("</table> <br> <br>");
-                // end of: taulun rakentaminen ja populointi 
-            // end of: sisälooppi
-           
-
-          } // ulkolooppi
-          // echo("</table> <br> <br>");
-          // end of: taulun rakentaminen ja populointi 
-
-       
+              echo ("</tr>");
+          }
+          echo("</table> <br> <br>");
+          // end of: taulun rakentaminen ja populointi           
       
       /* free result set */
       $result->free();
@@ -173,60 +126,53 @@
         <strong>
 
             <?php
+              session_start();
 
-            session_start();
+                  // $myusername = $_SESSION['login_user'];             
 
-            // print_r($_SESSION);
+                  // include("db_connection_config.php");
+                  
+                  // $sql = "SELECT b.henkiloID, b.sukunimi, b.etunimi
+                  // ,c.asiakasID, c.nimi, c.tyyppi, f.rooliID, f.rooli
+                  // FROM kayttaja a
+                  // inner join henkilo b on (a.kayttajatunnus=b.sposti)
+                  // inner join asiakas c on (b.asiakasID=c.asiakasID)
+                  // inner join henkilonRooli e on (b.henkiloID=e.henkiloID)
+                  // inner join rooli f on (e.rooliID=f.rooliID)
+                  // WHERE a.kayttajatunnus = '$myusername'";
+                  
+                  // $result = mysqli_query($mysqli,$sql);
+                  // $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 
-            $myusername = $_SESSION['login_user'];
+                  // echo($row['rooli'] . " "  . $row['sukunimi'] . " " . $row['etunimi']);
 
-            include("db_connection_config.php");
-            
-            $sql = "SELECT b.henkiloID, b.sukunimi, b.etunimi
-            ,c.asiakasID, c.nimi, c.tyyppi, f.rooliID, f.rooli
-            FROM kayttaja a
-            inner join henkilo b on (a.kayttajatunnus=b.sposti)
-            inner join asiakas c on (b.asiakasID=c.asiakasID)
-            inner join henkilonRooli e on (b.henkiloID=e.henkiloID)
-            inner join rooli f on (e.rooliID=f.rooliID)
-            WHERE a.kayttajatunnus = '$myusername'";
-            
-            $result = mysqli_query($mysqli,$sql);
-            $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-
-            // $count = mysqli_num_rows($result);
-            // echo("Rivimäärä: " . $count);
-
-            // $_SESSION['henkilo_ID']=$row['henkiloID'];
-
-            echo($row['rooli'] . " "  . $row['sukunimi'] . " " . $row['etunimi']);
-
+              echo($_SESSION['asiakas_nimi'] . ", "  . $_SESSION['sukunimi'] . " " . $_SESSION['etunimi']);
             ?>        
     
         </strong>
 
     </h4>
 
-    <button class="accordion"><strong>Oma henkilöstö</strong></button>
+<button class="accordion"><strong>Oma henkilöstö</strong></button>
 <div class="panel">
   <p id="root2">
     <?php 
-        // Tähän työntekijät tietokannasta   
-        require 'db_connection_config.php';       
-        // seuraava looppi
-        $sql = "SELECT a.henkiloId, concat(a.sukunimi, ' ', a.etunimi) as Nimi
-         FROM henkilo a 
-         inner join asiakas b on (a.asiakasID=b.asiakasID)
-         inner join henkilonRooli c on (a.henkiloId=c.henkiloId)
-         inner join rooli e on (c.rooliId=e.rooliId)
-         where b.tyyppi='O' 
-         and e.rooli='Projektipäällikkö'      
-         ";
+
+        session_start();
+        $henkiloID=$_SESSION['henkilo_ID'];
+        $asiakasID=$_SESSION['asiakas_ID'];          
+
+        require 'db_connection_config.php'; 
+
+        $sql = "SELECT a.henkiloID, concat(a.sukunimi, ' ',a.etunimi) as Nimi, a.sposti
+        FROM henkilo a
+        WHERE a.asiakasID=$asiakasID
+        and exists(select 1 from henkilo ab where a.asiakasID=ab.asiakasID and ab.henkiloID=$henkiloID);           
+        ";
 
         $result = mysqli_query($mysqli, $sql) or die ("Bad Query: $sql");
 
-        // taulun rakentaminen ja populointi
-        echo("<h3>Projektipäälliköt </h3>");
+        // echo("<h3>Oma henkilöstö </h3>");
         echo("<table border=0>");
         /* Get field information for all columns */
         $finfo = mysqli_fetch_fields($result);
@@ -236,32 +182,7 @@
             foreach ($finfo as $val) {echo ("<td>{$row[$val->name]}</td>");}
             echo ("</tr>");
         }
-        echo("</table> <br> <br>");
-
-        // seuraava looppi
-        $sql = "SELECT a.henkiloId, concat(a.sukunimi, ' ', a.etunimi) as Nimi
-         FROM henkilo a 
-         inner join asiakas b on (a.asiakasID=b.asiakasID)
-         inner join henkilonRooli c on (a.henkiloId=c.henkiloId)
-         inner join rooli e on (c.rooliId=e.rooliId)
-         where b.tyyppi='O' 
-         and e.rooli='Työntekijä'      
-         ";
-
-        $result = mysqli_query($mysqli, $sql) or die ("Bad Query: $sql");
-
-        // taulun rakentaminen ja populointi
-        echo("<h3>Työntekijät </h3>");
-        echo("<table border=0>");
-        /* Get field information for all columns */
-        $finfo = mysqli_fetch_fields($result);
-        foreach ($finfo as $val) {echo ('<th>'.$val->name.'</th>');}
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo ("<tr>");
-            foreach ($finfo as $val) {echo ("<td>{$row[$val->name]}</td>");}
-            echo ("</tr>");
-        }
-        echo("</table> <br> <br>");        
+        echo("</table> <br> <br>");    
 
       /* free result set */
       $result->free();
@@ -271,50 +192,20 @@
     ?>   
 
   </p>
-  <button class="open-button" onclick="Form1()">Add User</button>
+  <!-- <button class="open-button" onclick="Form1()">Add User</button> -->
 </div>
 
-<button class="accordion"><strong>Projektit</strong></button>
+<button class="accordion"><strong>Projektit - tarvitaanko?</strong></button>
 <div class="panel">
-  <p>Esimerkki projekti</p>
-  <button class="open-button" onclick="Form1()">Add Project</button>
+  <p></p>
+ <!--  <button class="open-button" onclick="Form1()">Add Project</button> -->
 </div>
 
-<button class="accordion"><strong>Asiakkaat</strong></button>
+<button class="accordion"><strong>Asiakkaat - tarvitaanko?</strong></button>
 <div class="panel">
   <p id='plAsiakkaat'>
-  <?php 
-        // Tähän työntekijät tietokannasta   
-        require 'db_connection_config.php';       
-        // seuraava looppi
-        $sql = "SELECT a.asiakasId, a.nimi
-         FROM asiakas a
-         where a.tyyppi='T'     
-         ";
-
-        $result = mysqli_query($mysqli, $sql) or die ("Bad Query: $sql");
-
-        // taulun rakentaminen ja populointi
-        echo("<h3>Asiakkaat </h3>");
-        echo("<table border=0>");
-        /* Get field information for all columns */
-        $finfo = mysqli_fetch_fields($result);
-        foreach ($finfo as $val) {echo ('<th>'.$val->name.'</th>');}
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo ("<tr>");
-            foreach ($finfo as $val) {echo ("<td>{$row[$val->name]}</td>");}
-            echo ("</tr>");
-        }
-        echo("</table> <br> <br>");
-
-      /* free result set */
-      $result->free();
-      
-      /* close connection */
-      $mysqli->close();        
-    ?>   
   </p>
-  <button class="open-button" onclick="Form1()">Add User</button>
+  <!--  <button class="open-button" onclick="Form1()">Add User</button> -->
 </div>
 
 
