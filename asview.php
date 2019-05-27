670297dc45b54d2e91c356933ad2ce141a734a7c
jaamer71 <jaamer71@gmail.com>
2019-05-27 10:32:08 +0300

    Footer + social media links

--- asview.php
+++ asview.php
@@ -211,10 +211,19 @@
   <!--  <button class="open-button" onclick="Form1()">Add User</button> -->
 </div>
 
-
-
 <script src="view.js"></script>
 
+<!-- footerin määrittely -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<div class="footer">
+
+
+<!-- sosiaalisen median linkit -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<a href="https://www.facebook.com" class="fa fa-facebook"></a>
+<a href="https://www.twitter.com" class="fa fa-twitter"></a>
+<a href="https://www.linkedin.com" class="fa fa-linkedin"></a>
+</div>
 
 </body>
 </html>
