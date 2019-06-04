8941e25838a2e4c9af065251ee66d184e06352cf
jaamer71 <jaamer71@gmail.com>
2019-06-04 09:38:56 +0300

    sosiaalisen median kuvakkeet

--- asview.php
+++ asview.php
@@ -220,12 +220,8 @@
 
 <script src="view.js"></script>
 
-<!-- footerin määrittely -->
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- sosiaalisen median linkit + footer -->
 <div class="footer">
-
-
-<!-- sosiaalisen median linkit -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <a href="https://www.facebook.com" class="fa fa-facebook"></a>
 <a href="https://www.twitter.com" class="fa fa-twitter"></a>
--- ttview.php
+++ ttview.php
@@ -324,11 +324,12 @@
 
 <script src="view.js"></script>
 
-<!-- footerin määrittely -->
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<div class="footer">
+<!-- footerin määrittely 
+<meta name="viewport" content="width=device-width, initial-scale=1"> -->
+
 
 <!-- sosiaalisen median linkit -->
+<div class="footer">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <a href="https://www.facebook.com" class="fa fa-facebook"></a>
 <a href="https://www.twitter.com" class="fa fa-twitter"></a>
