982f8f413a25608bc176e87f2002668db1a191cf
jaamer71 <jaamer71@gmail.com>
2019-06-04 10:25:45 +0300

    sosiaalisen median kuvakkeet

--- ttview.php
+++ ttview.php
@@ -320,15 +320,9 @@
   <button class="open-button" onclick="Form1()">Add User</button>
 </div>
 
-
-
 <script src="view.js"></script>
 
-<!-- footerin määrittely 
-<meta name="viewport" content="width=device-width, initial-scale=1"> -->
-
-
-<!-- sosiaalisen median linkit -->
+<!-- sosiaalisen median linkit + footer -->
 <div class="footer">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <a href="https://www.facebook.com" class="fa fa-facebook"></a>
